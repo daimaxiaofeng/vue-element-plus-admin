ece26ce0f550a16ce5c67dc1aaceb1072f32427a
daimaxiaofeng <daimaxiaofeng@qq.com>
2024-01-09 22:25:21 +0800

    wip: remove some navigation bar ICONS

--- src/store/modules/app.ts
+++ src/store/modules/app.ts
@@ -44,9 +44,9 @@ export const useAppStore = defineStore('app', {
       collapse: false, // 折叠菜单
       uniqueOpened: false, // 是否只保持一个子菜单的展开
       hamburger: true, // 折叠图标
-      screenfull: true, // 全屏图标
-      size: true, // 尺寸图标
-      locale: true, // 多语言图标
+      screenfull: false, // 全屏图标
+      size: false, // 尺寸图标
+      locale: false, // 多语言图标
       tagsView: true, // 标签页
       tagsViewIcon: true, // 是否显示标签图标
       logo: true, // logo
